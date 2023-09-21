eb02d1eda22a7357955807d3bd2b795fe430b220
John <ADJordan305@hotmail.com>
2023-09-20 22:14:45 -0500

    image shit

--- src/commands/tools/image_generater.js
+++ src/commands/tools/image_generater.js
@@ -1,19 +1,31 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const Canvas = require('@napi-rs/canvas');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("generate")
     .setDescription("Generates an image with the users information"),
   async execute(interaction, client) {
+    const canvas = Canvas.createCanvas(700, 250);
+    const context = canvas.getContext('2d');
+	  const background = await Canvas.loadImage(`../../../BotImage.png`);
+
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+  
+  
     const embed = new EmbedBuilder()
-      .setColor('Red')
-      .setTitle(`Bot Shutting Down`)
+      .setColor('Blue')
+      .setTitle(`test`)
       .setTimestamp(Date.now())
       .setFooter({
         text: `Requested By: ${interaction.user.username}`
       })
     await interaction.reply({
-      embeds: [embed], ephemeral: true
+      files: [attachment],
+      embeds: [embed],
+      ephemeral: true
     });
   },
 };
